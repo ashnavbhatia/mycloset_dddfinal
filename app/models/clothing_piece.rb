495910fbacf36be6b4f0400b81433048000ae784
anonymous anonymous <ashnavbhatia@gmail.com>
2022-03-12 20:27:06 +0000

    Validate presence of clothing_pieces.title

--- app/models/clothing_piece.rb
+++ app/models/clothing_piece.rb
@@ -5,6 +5,8 @@ class ClothingPiece < ApplicationRecord
 
   # Validations
 
+  validates :title, :presence => true
+
   validates :user_id, :presence => true
 
   # Scopes
--- spec/models/clothing_piece_spec.rb
+++ spec/models/clothing_piece_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe ClothingPiece, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:title) }
+
     it { should validate_presence_of(:user_id) }
 
     end
